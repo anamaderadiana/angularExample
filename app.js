2aae6a5d62d6e1bfb338c962c26544bfef67aa69
Marta Madera <martamadera@MacBook-Air-de-Marta.local>
2016-06-29 13:29:16 +0200

    Button show

--- app.js
+++ app.js
@@ -6,4 +6,35 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', function($scope){
     $scope.message = "Hello";
+    $scope.people = [
+        {
+            name:"Peter",
+            age:20
+        },
+        {
+            name:"Maria",
+            age:15
+        },
+        {
+            name:"Jim",
+            age: 30
+        }
+    ];
+    
+    $scope.name = '';
+    
+    $scope.addPerson = function (){
+    
+        var person = {
+            name: $scope.name,
+            age: $scope.age
+        };
+        $scope.people.push(person);
+    };
+    
+    $scope.isFilled = function(){
+        
+        return $scope.name.length > 0 && $scope.age.length > 0;
+    
+    };
 });
